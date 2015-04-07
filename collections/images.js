54e07e9aba7bd9073c3733afdf4ccc3c3b28b69a
alexpeissel <alexpeissel@googlemail.com>
2015-04-07 12:47:39 +0100

    Added key file structure and simple gallery functionality

---
+++ collections/images.js
@@ -0,0 +1,68 @@
+//Set Cache Control headers so we don't overload our meteor server with http requests
+FS.HTTP.setHeadersForGet([
+    ['Cache-Control', 'public, max-age=31536000']
+]);
+
+//Create the master store
+var masterStore = new FS.Store.GridFS("master");
+
+//Create a thumbnail store
+var thumbnailStore = new FS.Store.GridFS("thumbnail", {
+    //Create the thumbnail as we save to the store.
+    transformWrite: function(fileObj, readStream, writeStream) {
+        /* Use graphicsmagick to create a 300x300 square thumbnail at 100% quality,
+         * orient according to EXIF data if necessary and then save by piping to the
+         * provided writeStream */
+        gm(readStream, fileObj.name).resize(300,300,"^")
+            .gravity('Center').crop(300, 300).quality(100).autoOrient().stream().pipe(writeStream);
+    }
+});
+
+//Create globally scoped Images collection.
+Images = new FS.Collection("images", {
+    stores: [thumbnailStore, masterStore],
+    filter: {
+        maxSize: 10485760, //in bytes
+        allow: {
+            contentTypes: ['image/*'],
+            extensions: ['png', 'jpg', 'jpeg', 'gif']
+        },
+        onInvalid: function (message) {
+            if(Meteor.isClient){
+                alert(message);
+            }else{
+                console.warn(message);
+            }
+        }
+    }
+});
+
+
+//Use allow to control insert, update, remove and download. In this case we will just allow them all.
+Images.allow({
+    insert: function(userId, file) {
+        return true;
+    },
+    update: function(userId, file, fields, modifier) {
+        return true;
+    },
+    remove: function(userId, file) {
+        return true;
+    },
+    download: function() {
+        return true;
+    }
+});
+
+
+//If we're on the server publish the collection, otherwise we are on the client and we should subscribe to the publication.
+if(Meteor.isServer){
+
+    Meteor.publish('images', function () {
+        return Images.find();
+
+    });
+
+}else{
+    Meteor.subscribe('images');
+}
--- mass.js
+++ mass.js
@@ -1,37 +1,125 @@
 if (Meteor.isClient) {
 
+  Template.detector.rendered = function(){
+    console.log("rendered canvas");
+    var canvas = document.getElementById("detectorCanvas");
+    var context = canvas.getContext("2d");
 
-  Template.setup.rendered = function() {
-    //var script = document.createElement('script');
-    //script.type = 'text/javascript';
-    //script.async = true;
-    //
-    //script.src = "lib/aruco.js";
-    //
-    //var target = document.getElementsByTagName('script')[0];
-    //target.parentNode.insertBefore(script, target);
-    //console.log("Injected script");
+    var image = new Image();
+    image.src = "/images/pic1.jpg";
+
+    context.drawImage(image, 0, 0, canvas.width, canvas.height);
   }
 
   Template.detector.events({
     'click': function () {
-      console.log("You clicked something");
 
-      //var canvas = document.getElementById("detectorCanvas");
-      //var context = canvas.getContext("2d");
-      //var canvas.width = parseInt(canvas.style.width);
-      //var canvas.height = parseInt(canvas.style.height);
+      canvas = document.getElementById("detectorCanvas");
+      context = canvas.getContext("2d");
+      canvas.width = parseInt(canvas.style.width);
+      canvas.height = parseInt(canvas.style.height);
+      console.log("Canvas found with width: " + canvas.width + " Height: " + canvas.height);
 
-      CV.BlurStack();
+      var modelSize = 35.0; //millimeters
+
+      var validImage = {
+        imageData: "dataGoesHere",
+        rotationX: null,
+        rotationY: null,
+        rotationZ: null,
+        positionX: null,
+        positionY: null,
+        positionZ: null
+      }
+
       detector = new AR.Detector();
       posit = new POS.Posit(modelSize, canvas.width);
 
+      var image = new Image();
+      image.src = "/images/pic1.jpg";
+
+      context.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+      imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+      var markers = detector.detect(imageData);
+      updateScenes(markers);
+
+      function updateScenes(markers){
+        var corners, corner, pose, i;
+
+        if (markers.length > 0){
+          corners = markers[0].corners;
+
+          for (i = 0; i < corners.length; ++ i){
+            corner = corners[i];
+
+            corner.x = corner.x - (canvas.width / 2);
+            corner.y = (canvas.height / 2) - corner.y;
+          }
+
+          pose = posit.pose(corners);
+
+          updateObject(validImage, pose.bestRotation, pose.bestTranslation, pose.bestError);
+
+        } else {
+          console.log("No markers detected");
+        }
+
+      }
+
+      function updateObject(object, rotation, translation, error) {
+        var yaw = -Math.atan2(rotation[0][2], rotation[2][2]);
+        var pitch = -Math.asin(-rotation[1][2]);
+        var roll = Math.atan2(rotation[1][0], rotation[1][1]);
+        object.rotationX = -Math.asin(-rotation[1][2]);
+        object.rotationY = -Math.atan2(rotation[0][2], rotation[2][2]);
+        object.rotationZ = Math.atan2(rotation[1][0], rotation[1][1]);
+
+        object.positionX = translation[0];
+        object.positionY = translation[1];
+        object.positionZ = -translation[2];
+
+        console.log(object.rotationX);
+        console.log(object.rotationY);
+        console.log(object.rotationZ);
+        console.log(object.positionX);
+        console.log(object.positionY);
+        console.log(object.positionZ);
+      }
+
     }
   });
+
+  Template.uploader.events({
+    'change #fileInput': function(event, template) {
+     console.log("Registered upload");
+      FS.Utility.eachFile(event, function(file) {
+        var newFile = new FS.File(file);
+        file.metadata = {
+          title: "",
+          rotationX: "",
+          rotationY: "",
+          rotationZ: "",
+          positionX: "",
+          positionY: "",
+          positionZ: ""
+        };
+        Images.insert(file, function (err, fileObj) {
+          // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+          console.log("Uploaded file: " + file.name);
+        });
+      });
+    }
+  });
+
+  Template.images.images = function() {
+    return Images.find();
+  };
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+
   });
 }
